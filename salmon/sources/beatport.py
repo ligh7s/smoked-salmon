f995ebc760321638e207f52ad0154ac29eb74946
bootldrDNB <bootldrDNB@gmail.com>
2021-04-09 13:24:39 +0100

    bugfix: beatport has stopped using their 'pro' subdomain, using just beatport.com instead to fix bug where salmon was unable to fetch metadata from beatport

--- salmon/sources/beatport.py
+++ salmon/sources/beatport.py
@@ -5,9 +5,9 @@ from salmon.sources.base import BaseScraper
 
 class BeatportBase(BaseScraper):
 
-    url = site_url = "https://pro.beatport.com"
-    search_url = "https://pro.beatport.com/search/releases"
+    url = site_url = "https://beatport.com"
+    search_url = "https://beatport.com/search/releases"
     release_format = "/release/{rls_name}/{rls_id}"
     regex = re.compile(
-        r"^https?://(?:(?:www|pro|classic)\.)?beatport\.com/release/.+?/(\d+)/?$"
+        r"^https?://(?:(?:www|classic)\.)?beatport\.com/release/.+?/(\d+)/?$"
     )
